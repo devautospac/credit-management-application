a5075ab3b692fb1c6a890fc5cf4819cfafe55790
rayen benhassen <rayenbenhassen07@gmail.com>
2024-08-13 12:49:09 +0100

    first committtt

--- src/components/Test.jsx
+++ src/components/Test.jsx
@@ -14,16 +14,5 @@ export default function Test({ currentUser }) {
     }
   }, [currentUser, isLoggedIn, router]);
 
-  return (
-    <div className="p-4 flex justify-start items-center gap-2">
-      <div
-        onClick={() => {
-          signOut();
-        }}
-        className="mt-4 px-4 py-2 bg-red-500 text-white rounded hover:bg-red-700 cursor-pointer w-32 "
-      >
-        Déconnexion
-      </div>
-    </div>
-  );
+  return <div className="p-4 flex justify-start items-center gap-2"></div>;
 }
